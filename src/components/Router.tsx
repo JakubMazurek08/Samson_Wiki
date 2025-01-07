b86aabd77ebc9a03dbfbf64d3b17d7b13d065325
AzureAD\MichałKarwacki <jan.kowalski@example.com>
2025-01-07 10:54:45 +0100

    Error page has been modified

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,6 +1,8 @@
 import {createBrowserRouter,RouterProvider} from "react-router-dom";
 import {Home} from "../pages/Home.tsx";
 import {NavBar} from "../pages/NavBar.tsx";
+import {NewExercisePage} from "../pages/NewExercisePage.tsx";
+import {ErrorPage} from "../pages/errorPage.tsx";
 
 const router = createBrowserRouter([
     {
--- src/pages/errorPage.tsx
+++ src/pages/errorPage.tsx
@@ -2,15 +2,13 @@ import {Link} from "react-router-dom";
 
 export const ErrorPage = () => {
 
-
-
     return (
         <div>
             <div className="flex flex-col items-center justify-center bg-primary-light min-h-screen">
                 <h1 className="text-6xl text-transform: uppercase text-white">Something went wrong...</h1>
                 <Link to="/" className="text-2xl text-transform: uppercase text-white bg-primary-medium px-10 py-3 mt-4 rounded-md">Main Page</Link>
             </div>
-            <Link to="https://www.instagram.com/sentinobln/" className="absolute bottom-4 left-4 text-white text-primary-light" target="_blank">Skibidi</Link>
+            <Link to="https://www.instagram.com/sentinobln/" className="absolute bottom-4 left-4  text-primary-light" target="_blank">Skibidi</Link>
         </div>
     )
 }
