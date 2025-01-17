53207e1c567e20e63e5efd79d78ad28344d4e879
AzureAD\MichałKarwacki <jan.kowalski@example.com>
2025-01-17 10:07:49 +0100

    sigma errorPage

--- src/pages/errorPage.tsx
+++ src/pages/errorPage.tsx
@@ -8,7 +8,7 @@ export const ErrorPage = () => {
                 <h1 className="text-6xl text-transform: uppercase text-white">Something went wrong...</h1>
                 <Link to="/" className="text-2xl text-transform: uppercase text-white bg-primary-medium px-10 py-3 mt-4 rounded-md">Main Page</Link>
             </div>
-            <Link to="https://www.instagram.com/sentinobln/" className="absolute bottom-4 left-4  text-primary-light" target="_blank">Skibidi</Link>
+            <Link to="https://www.instagram.com/sentinobln/" className="absolute bottom-4 left-4 text-primary-light" target="_blank">Skibidi</Link>
         </div>
     )
 }
